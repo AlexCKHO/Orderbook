ae7778e7017a514904cd12fcac3053104defa047
AlexCKHO <alexho.ofl@gmail.com>
2026-01-16 23:52:02 +0000

    Created Order, OrderType, and Side

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use crate::models::order::Order;
+
+mod models;
+
 fn main() {
+
     println!("Hello, world!");
 }
---
+++ src/models/order.rs
@@ -0,0 +1,22 @@
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum Side {
+    Bid,
+    Ask,
+}
+
+// 2. 定義訂單類型
+#[derive(Debug, Clone, Copy, PartialEq)]
+pub enum OrderType {
+    Limit,
+    Market,
+}
+
+#[derive(Debug, Clone)]
+pub struct Order {
+    pub id: u64,
+    pub price: u64,
+    pub qty: u64,
+    pub side: Side,
+    pub order_type: OrderType,
+
+}
