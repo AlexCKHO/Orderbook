3fbc8ecde2dcd27943e4aba5f127fa7be21703d4
AlexCKHO <alexho.ofl@gmail.com>
2026-01-17 22:44:47 +0000

    Added pseudocode

--- src/models/orderBook.rs
+++ src/models/orderBook.rs
@@ -6,11 +6,56 @@ struct OrderBook {
 }
 
 impl OrderBook {
-    pub fn add_limit_order(&mut self, order: Order) {
-        if order.side == Side::Bid {
-            self.bids.push(order)
-        } else if order.side == Side::Ask {
-            self.asks.push(order)
+
+    fn add_order(&mut self, order: Order) {
+
+        if order.order_type == OrderType::Limit {
+            if order.side == Side::Bid {
+
+
+            } else if order.side == Side::Ask {
+
+            }
         }
+
+        else if order.order_type == OrderType:: Market{
+            todo!("OrderType:: Market");
+
+
+        }
+
     }
+
+    // When people are buying
+    fn match_new_bid(&mut self, new_bid_order: Order) {
+        if let Some(smallest_ask_order) =  self.asks.last_mut() {
+            if new_bid_order.price == smallest_ask_order.price {
+                // Balancing biding and asking qty
+                // If new_bid_order.qty > smallest_ask_order.qty
+                    // new_bid_order.qty -= smallest_ask_order.qty
+                    // smallest_ask_order gone from bids
+                // Else if new_bid_order.qty < smallest_ask_order.qty
+                    // smallest_ask_order.qty -= new_bid_order.qty
+                    // new_bid_order gone
+            }
+
+        } else {
+
+
+        }
+
+
+
+
+    }
+
+    pub fn add_order_to_bids_in_order(&mut self, order: Order) {
+
+    }
+
+    pub fn add_order_to_asks_in_order(&mut self, order: Order) {
+
+    }
+
+
 }
