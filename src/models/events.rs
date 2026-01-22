117ca07b57464d1ba585ca92b20c8766d54958fd
AlexCKHO <alexho.ofl@gmail.com>
2026-01-22 07:14:08 +0000

    Added comments

--- src/models/events.rs
+++ src/models/events.rs
@@ -3,7 +3,7 @@ use crate::models::order::Side;
 #[derive(Debug, Clone, Copy, PartialEq)]
 pub enum MatchEvent {
     OrderPlaced {id: u64, price: u64, qty: u64, side: Side},
-    TradeExecuted {maker_id: u64, taker_id: u64, price: u64, qty: u64, timestamp: i64},
+    TradeExecuted {maker_id: u64, taker_id: u64, price: u64, qty: u64, timestamp: i64}, //Timestamp Unix Timestamp in Nanoseconds for record purpose
     OrderCancelled {id: u64, cancelled_qty: u64},
     OrderKilled {id: u64, killed_qty: u64},
 }
--- src/models/order.rs
+++ src/models/order.rs
@@ -18,6 +18,18 @@ pub struct Order {
     pub qty: u64,
     pub side: Side,
     pub order_type: OrderType,
+    // Notes: Why timestamp:
+    
+    // 1. Audit Trail & Dispute Resolution
+    // If two orders are submitted at the same price (e.g., both at $100), why was Order A filled while Order B remained unfilled? A timestamp is essential to prove that Order A arrived before Order B, thereby validating the integrity of the FIFO (First-In-First-Out) / Price-Time Priority logic.
+    //
+    // 2. Latency Analysis & Performance Metrics
+    // In High-Frequency Trading (HFT), benchmarking performance is critical. You need to answer: "How much time did the engine take to match this order?"
+    //
+    // Formula: Time(Trade Execution Event) - Time(Order Arrival) = System Latency. Without an input timestamp, it is impossible to calculate this delta or determine if the engine is performing efficiently.
+    //
+    // 3. Debugging & Replay
+    // During periods of high market volatility or extreme throughput, logs can become chaotic. Without precise timestamps, it is nearly impossible to reconstruct the exact sequence of events or determine which specific order triggered a trade.
     pub timestamp: i64
 }
 
