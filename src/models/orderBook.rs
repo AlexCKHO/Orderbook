1dcff5589fa816d45a5a8823019e5e6bede25035
AlexCKHO <alexho.ofl@gmail.com>
2026-01-22 22:05:29 +0000

    Added events in match_new_market_bid, add order to bids and add order to asks

--- src/models/orderBook.rs
+++ src/models/orderBook.rs
@@ -1,6 +1,8 @@
+use crate::models::events::MatchEvent;
 use crate::models::order::{Order, OrderEntry, OrderType, Side};
 
 struct OrderBook {
+    // FIFO;
     bids: Vec<OrderEntry>,
     asks: Vec<OrderEntry>,
 }
@@ -23,7 +25,7 @@ impl OrderBook {
     }
 
     // When people are buying
-    fn match_new_limit_bid(&mut self, mut new_bid_order: Order) {
+    fn match_new_limit_bid(&mut self, mut new_bid_order: Order, event: &mut Vec<MatchEvent>) {
         // Loop until:
         // 1. Finish matching new_bid_order.qty == 0
         // 2. Asks is empty
@@ -42,6 +44,7 @@ impl OrderBook {
 
             if best_ask.qty > new_bid_order.qty {
                 best_ask.qty -= new_bid_order.qty;
+
                 new_bid_order.qty = 0;
             } else {
                 // For == and <, all need to pop
@@ -51,11 +54,11 @@ impl OrderBook {
         }
 
         if new_bid_order.qty > 0 {
-            self.add_order_to_bids_in_order(new_bid_order)
+            self.add_order_to_bids(new_bid_order)
         }
     }
 
-    fn match_new_limit_ask(&mut self, mut new_ask_order: Order) {
+    fn match_new_limit_ask(&mut self, mut new_ask_order: Order, events: &mut Vec<MatchEvent>) {
         // Loop until:
         // 1. Finish matching new_ask_order.qty == 0
         // 2. Bids is empty, no orders to be matched
@@ -84,11 +87,11 @@ impl OrderBook {
         }
 
         if new_ask_order.qty > 0 {
-            self.add_order_to_asks_in_order(new_ask_order)
+            self.add_order_to_asks(new_ask_order)
         }
     }
 
-    fn match_new_market_bid(&mut self, mut new_bid_order: Order) {
+    fn match_new_market_bid(&mut self, mut new_bid_order: Order, events: &mut Vec<MatchEvent>) {
         // Loop until:
         // 1. Finish matching new_bid_order.qty == 0
         // 2. Asks is empty
@@ -101,10 +104,31 @@ impl OrderBook {
 
             if best_ask.qty > new_bid_order.qty {
                 best_ask.qty -= new_bid_order.qty;
+
+                events.push(MatchEvent::TradeExecuted {
+                    maker_id: best_ask.id,
+                    taker_id: new_bid_order.id,
+                    price: best_ask.price,
+                    qty: new_bid_order.qty,
+                    // ToDO: fix the timestamp to Unix Timestamp
+                    timestamp: new_bid_order.timestamp,
+                });
+
                 new_bid_order.qty = 0;
+
                 break;
             } else {
                 new_bid_order.qty -= best_ask.qty;
+
+                events.push(MatchEvent::TradeExecuted {
+                    maker_id: best_ask.id,
+                    taker_id: new_bid_order.id,
+                    price: best_ask.price,
+                    qty: best_ask.qty,
+                    // ToDO: fix the timestamp to Unix Timestamp
+                    timestamp: new_bid_order.timestamp,
+                });
+
                 self.asks.pop();
             }
         }
@@ -113,14 +137,14 @@ impl OrderBook {
         // and the asks can't fulfil the bid.qty, just kill the order
         // Standard Immediate-Or-Cancel approach
         if new_bid_order.qty > 0 {
-            println!(
-                "Market Order partially filled. Remaining {} qty killed.",
-                new_bid_order.qty
-            );
+            events.push(MatchEvent::OrderKilled {
+                id: new_bid_order.id,
+                killed_qty: new_bid_order.qty,
+            })
         }
     }
 
-    fn match_new_market_ask(&mut self, mut new_ask_order: Order) {
+    fn match_new_market_ask(&mut self, mut new_ask_order: Order, events: &mut Vec<MatchEvent>) {
         // Loop until:
         // 1. Finish matching new_ask_order.qty == 0
         // 2. Bids is empty
@@ -134,7 +158,7 @@ impl OrderBook {
             if best_bid.qty > new_ask_order.qty {
                 best_bid.qty -= new_ask_order.qty;
                 new_ask_order.qty = 0;
-                break
+                break;
             } else {
                 new_ask_order.qty -= best_bid.qty;
                 self.bids.pop();
@@ -153,7 +177,7 @@ impl OrderBook {
         }
     }
 
-    pub fn add_order_to_bids_in_order(&mut self, new_bid_order: Order) {
+    pub fn add_order_to_bids(&mut self, new_bid_order: Order, events: &mut Vec<MatchEvent>) {
         if new_bid_order.qty > 0 {
             // Bids are sorted in Ascending order: [Smallest Price ... Highest Price].
             // Example: [98, 99, 100(Old)]
@@ -170,10 +194,17 @@ impl OrderBook {
             let index = self.bids.partition_point(|x| x.price < new_bid_order.price);
 
             self.bids.insert(index, OrderEntry::new(&new_bid_order));
+
+            events.push(MatchEvent::OrderPlaced {
+                id: new_bid_order.id,
+                price: new_bid_order.price,
+                qty: new_bid_order.qty,
+                side: new_bid_order.side,
+            });
         }
     }
 
-    pub fn add_order_to_asks_in_order(&mut self, new_ask_order: Order) {
+    pub fn add_order_to_asks(&mut self, new_ask_order: Order, events: &mut Vec<MatchEvent>) {
         if new_ask_order.qty > 0 {
             // Asks are sorted in Descending order: [Highest Price ... Smallest Price].
             // Example: [102, 101, 100(Old)]
@@ -191,6 +222,13 @@ impl OrderBook {
             let index = self.asks.partition_point(|x| x.price > new_ask_order.price);
 
             self.asks.insert(index, OrderEntry::new(&new_ask_order));
+
+            events.push(MatchEvent::OrderPlaced {
+                id: new_ask_order.id,
+                price: new_ask_order.price,
+                qty: new_ask_order.qty,
+                side: new_ask_order.side,
+            });
         }
     }
 }
@@ -200,7 +238,14 @@ mod tests {
     use super::*;
 
     /// Helper function to construct Orders to reduce boilerplate code in tests.
-    fn new_order(id: u64, price: u64, qty: u64, side: Side, order_type: OrderType, timestamp: i64) -> Order {
+    fn new_order(
+        id: u64,
+        price: u64,
+        qty: u64,
+        side: Side,
+        order_type: OrderType,
+        timestamp: i64,
+    ) -> Order {
         Order {
             id,
             price,
@@ -262,10 +307,24 @@ mod tests {
         };
 
         // User A places Bid @ 100 (First arrival)
-        ob.add_order(new_order(1, 100, 10, Side::Bid, OrderType::Limit, 1768032384123456789));
+        ob.add_order(new_order(
+            1,
+            100,
+            10,
+            Side::Bid,
+            OrderType::Limit,
+            1768032384123456789,
+        ));
 
         // User B places Bid @ 100 (Arrives later)
-        ob.add_order(new_order(2, 100, 10, Side::Bid, OrderType::Limit, 1768052384123456789));
+        ob.add_order(new_order(
+            2,
+            100,
+            10,
+            Side::Bid,
+            OrderType::Limit,
+            1768052384123456789,
+        ));
 
         // Expected Memory Layout (Bids are Ascending):
         // [100 (User B/New), 100 (User A/Old)]
@@ -273,7 +332,14 @@ mod tests {
 
         // Validation Logic:
         // An incoming Sell order @ 100, Qty 10 should completely fill User A, leaving User B.
-        ob.add_order(new_order(3, 100, 10, Side::Ask, OrderType::Limit, 1768082384123456789));
+        ob.add_order(new_order(
+            3,
+            100,
+            10,
+            Side::Ask,
+            OrderType::Limit,
+            1768082384123456789,
+        ));
 
         // The remaining order in the book should be User B (ID 2), honoring Time Priority.
         assert_eq!(ob.bids[0].id, 2);
@@ -288,11 +354,25 @@ mod tests {
 
         // 1. Seller A places Ask @ 100 (First arrival - "The Old Order")
         // ID: 1
-        ob.add_order(new_order(1, 100, 10, Side::Ask, OrderType::Limit, 1768032384123456789));
+        ob.add_order(new_order(
+            1,
+            100,
+            10,
+            Side::Ask,
+            OrderType::Limit,
+            1768032384123456789,
+        ));
 
         // 2. Seller B places Ask @ 100 (Later arrival - "The New Order")
         // ID: 2
-        ob.add_order(new_order(2, 100, 10, Side::Ask, OrderType::Limit, 1768052384123456789));
+        ob.add_order(new_order(
+            2,
+            100,
+            10,
+            Side::Ask,
+            OrderType::Limit,
+            1768052384123456789,
+        ));
 
         // --- Memory Layout Check (Mental Model) ---
         // Asks are sorted Descending: [Highest ... Lowest]
@@ -302,7 +382,14 @@ mod tests {
 
         // 3. Buyer C comes in to Buy 10 units @ 100
         // This should trigger a match against the "Best Ask".
-        ob.add_order(new_order(3, 100, 10, Side::Bid, OrderType::Limit, 1768082384123456789));
+        ob.add_order(new_order(
+            3,
+            100,
+            10,
+            Side::Bid,
+            OrderType::Limit,
+            1768082384123456789,
+        ));
 
         // --- Assertions ---
         // Buyer C should be fully filled.
@@ -327,14 +414,35 @@ mod tests {
 
         // 1. Setup Liquidity (Asks)
         // Seller A: Limit Sell @ 100, Qty 10 (Best Price)
-        ob.add_order(new_order(1, 100, 10, Side::Ask, OrderType::Limit,1768032384123456789));
+        ob.add_order(new_order(
+            1,
+            100,
+            10,
+            Side::Ask,
+            OrderType::Limit,
+            1768032384123456789,
+        ));
         // Seller B: Limit Sell @ 102, Qty 20 (Worse Price)
-        ob.add_order(new_order(2, 102, 20, Side::Ask, OrderType::Limit, 1768033384123456789));
+        ob.add_order(new_order(
+            2,
+            102,
+            20,
+            Side::Ask,
+            OrderType::Limit,
+            1768033384123456789,
+        ));
 
         // 2. Market Buy comes in (Qty 15)
-        // Expectation: It should consume the entire level at $100 (10 qty) 
+        // Expectation: It should consume the entire level at $100 (10 qty)
         // and partially consume the level at $102 (5 qty).
-        ob.add_order(new_order(3, 0, 15, Side::Bid, OrderType::Market, 1768033484123456789));
+        ob.add_order(new_order(
+            3,
+            0,
+            15,
+            Side::Bid,
+            OrderType::Market,
+            1768033484123456789,
+        ));
 
         // Check: The $100 price level should be fully consumed (Popped).
         assert_eq!(ob.asks.len(), 1);
@@ -347,7 +455,14 @@ mod tests {
         // 3. Huge Market Buy (Qty 1000)
         // Expectation: Consumes the remaining 15 units, then the rest of the order is killed (IOC).
         // It does NOT enter the queue.
-        ob.add_order(new_order(4, 0, 1000, Side::Bid, OrderType::Market, 1768033485123456789));
+        ob.add_order(new_order(
+            4,
+            0,
+            1000,
+            Side::Bid,
+            OrderType::Market,
+            1768033485123456789,
+        ));
 
         // Check: Asks should be completely empty.
         assert_eq!(ob.asks.len(), 0);
@@ -366,17 +481,38 @@ mod tests {
 
         // 1. Setup Liquidity (Bids)
         // Buyer A: Limit Buy @ 98, Qty 20 (Lower Price)
-        ob.add_order(new_order(1, 98, 20, Side::Bid, OrderType::Limit, 1768033384123456789));
+        ob.add_order(new_order(
+            1,
+            98,
+            20,
+            Side::Bid,
+            OrderType::Limit,
+            1768033384123456789,
+        ));
         // Buyer B: Limit Buy @ 100, Qty 10 (Higher/Best Price)
-        ob.add_order(new_order(2, 100, 10, Side::Bid, OrderType::Limit, 1768033484123456789));
+        ob.add_order(new_order(
+            2,
+            100,
+            10,
+            Side::Bid,
+            OrderType::Limit,
+            1768033484123456789,
+        ));
 
-        // Verify Layout: Bids are Ascending [98, 100]. 
+        // Verify Layout: Bids are Ascending [98, 100].
         // pop() takes from the end, so it should take $100 first.
 
         // 2. Market Sell comes in (Qty 15)
         // Expectation: It should hit the Best Bid ($100) first (eats 10),
         // then hit the Next Best Bid ($98) (eats 5).
-        ob.add_order(new_order(3, 0, 15, Side::Ask, OrderType::Market, 1768033584123456789));
+        ob.add_order(new_order(
+            3,
+            0,
+            15,
+            Side::Ask,
+            OrderType::Market,
+            1768033584123456789,
+        ));
 
         // Check: The $100 Bid should be fully consumed (Popped).
         // Only 1 bid level remains.
@@ -390,7 +526,14 @@ mod tests {
         // 3. Huge Market Sell (Qty 1000)
         // Expectation: Consumes the remaining 15 units at $98.
         // The rest of the sell order (985 qty) is Killed immediately (IOC).
-        ob.add_order(new_order(4, 0, 1000, Side::Ask, OrderType::Market, 1768033585123456789));
+        ob.add_order(new_order(
+            4,
+            0,
+            1000,
+            Side::Ask,
+            OrderType::Market,
+            1768033585123456789,
+        ));
 
         // Check: Orderbook should be completely empty.
         assert_eq!(ob.bids.len(), 0);
