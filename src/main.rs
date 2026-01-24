5d9ee4d118fc001a5b15a7368851b8f1882d48de
AlexCKHO <alexho.ofl@gmail.com>
2026-01-24 21:17:13 +0000

    Set up server

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
 use tokio;
+use tonic::transport::Server;
+use crate::orderbook_grpc::matching_engine_server::MatchingEngineServer;
+use crate::services::matching_engine_service::MatchingEngineService;
 
 mod orderbook_grpc {
     tonic::include_proto!("orderbook");
@@ -9,7 +12,24 @@ mod services;
 
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     println!("Hello, world!");
+
+    // A. Setting Address (Localhost : Port 50051)
+    let addr = "127.0.0.1:50051".parse()?;
+
+    //B. Setting up the service
+    // Here will call the MatchingEngineService::new() to initiate the OrderBook
+    let service = MatchingEngineService::new();
+
+    println!("🚀 Rust Matching Engine is listening on {}", addr);
+
+    Server::builder()
+        .add_service(MatchingEngineServer::new(service))
+        .serve(addr)
+        .await?;
+
+    Ok(())
+
 }
