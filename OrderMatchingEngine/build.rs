7aee45aa244ffa737395c02e584f37f57fd9d08a
AlexCKHO <alexho.ofl@gmail.com>
2026-01-25 11:38:00 +0000

    fix: updated ordermatching.proto file path

--- OrderMatchingEngine/build.rs
+++ OrderMatchingEngine/build.rs
@@ -1,4 +1,21 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("proto/orderbook.proto")?;
+    // 1. Define file paths
+    // "../Protos/orderbook.proto" indicates looking for the Protos folder in the parent directory
+    let proto_file = "../Protos/orderbook.proto";
+    let proto_dir = "../Protos";
+
+    // 2. [CRITICAL] Instruct Cargo to re-run this build script if the external file changes
+    // If you omit this line, Rust won't detect changes to the .proto file and will continue using the old code!
+    println!("cargo:rerun-if-changed={}", proto_file);
+
+    // 3. Compilation
+    // Using configure().compile() here provides more flexibility than the standard compile_protos()
+    tonic_build::configure()
+        .build_server(true) // We are the Server side, so this must be set to true
+        .compile_protos(
+            &[proto_file], // The list of files to compile
+            &[proto_dir],  // The root directory for imports (used to resolve imports if your proto file depends on others)
+        )?;
+
     Ok(())
 }
