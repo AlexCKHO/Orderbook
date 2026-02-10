6e9d04f48075276d937dc564e15fa254a3350df9
AlexCKHO <alexho.ofl@gmail.com>
2026-02-10 21:09:20 +0000

    feat: impl new match_new_limit_bid

--- OrderMatchingEngine/src/models/order_book.rs
+++ OrderMatchingEngine/src/models/order_book.rs
@@ -13,7 +13,6 @@ pub struct OrderBook {
 impl OrderBook {
     pub fn new() -> Self {
         OrderBook {
-
             bids: BTreeMap::new(),
             asks: BTreeMap::new(),
             order_locations: HashMap::new(),
@@ -46,263 +45,58 @@ impl OrderBook {
 
     // When people are buying
     fn match_new_limit_bid(&mut self, mut new_bid_order: Order, events: &mut Vec<MatchEvent>) {
-        // Loop until:
-        // 1. Finish matching new_bid_order.qty == 0
-        // 2. Asks is empty
-        // 3. Price mismatch, no ask price is lower or equal to
+       while new_bid_order.qty > 0 {
 
-        while new_bid_order.qty > 0 {
-            let best_ask = match self.asks.last_mut() {
-                Some(ask) => ask,
-                // Case 2. Asks is empty
-                None => break,
-            };
-            // Case 3. Price mismatch
-            if new_bid_order.price < best_ask.price {
-                break;
-            }
+           if let Some((&ask_price, ask_queue)) = self.asks.iter_mut().next() {
+               if ask_price > new_bid_order.price {
+                   break;
+               }
 
-            if best_ask.qty > new_bid_order.qty {
-                best_ask.qty -= new_bid_order.qty;
+               let best_ask_order = ask_queue.front_mut().unwrap();
 
-                events.push(MatchEvent::TradeExecuted {
-                    maker_id: best_ask.id,
-                    taker_id: new_bid_order.id,
-                    price: best_ask.price,
-                    qty: new_bid_order.qty,
-                    timestamp: new_bid_order.timestamp,
-                });
+               let match_qty = std::cmp::min(best_ask_order.qty, new_bid_order.qty);
 
-                new_bid_order.qty = 0;
-            } else {
-                // For == and <, all need to pop
-                new_bid_order.qty -= best_ask.qty;
+               new_bid_order.qty -= match_qty;
 
-                events.push(MatchEvent::TradeExecuted {
-                    maker_id: best_ask.id,
-                    taker_id: new_bid_order.id,
-                    price: best_ask.price,
-                    qty: best_ask.qty,
-                    timestamp: new_bid_order.timestamp,
-                });
+               if best_ask_order.qty == 0 {
+                   let removed_order = ask_queue.pop_front().unwrap();
+                   self.order_locations.remove(&removed_order.id);
+               }
 
-                self.asks.pop();
-            }
-        }
+               if ask_queue.is_empty() {
+                   self.asks.remove(&ask_price);
+               }
+
+           }else {
+               break;
+           }
+       }
 
         if new_bid_order.qty > 0 {
-            self.add_order_to_bids(new_bid_order, events);
+            self.bids.entry(new_bid_order.price).or_insert_with(VecDeque::new).push_back(new_bid_order.clone());
+            self.order_locations.insert(new_bid_order.id, (new_bid_order.price, Side::Bid));
         }
     }
 
     fn match_new_limit_ask(&mut self, mut new_ask_order: Order, events: &mut Vec<MatchEvent>) {
-        // Loop until:
-        // 1. Finish matching new_ask_order.qty == 0
-        // 2. Bids is empty, no orders to be matched
-        // 3. Price mismatch, no bid price is higher or equal to
 
-        while new_ask_order.qty > 0 {
-            let best_bid = match self.bids.last_mut() {
-                Some(bid) => bid,
-                // 2. Bids is empty,
-                None => break,
-            };
-            // 3. Price mismatch, no bid price is higher or equal to
-            // Even the best price people providing is smaller than new ask price
-            // there is no chance for matching, thus break
-            if new_ask_order.price > best_bid.price {
-                break;
-            }
-
-            if best_bid.qty > new_ask_order.qty {
-                best_bid.qty -= new_ask_order.qty;
-
-                events.push(MatchEvent::TradeExecuted {
-                    maker_id: best_bid.id,
-                    taker_id: new_ask_order.id,
-                    price: new_ask_order.price,
-                    qty: new_ask_order.qty,
-                    timestamp: new_ask_order.timestamp,
-                });
-
-                new_ask_order.qty = 0;
-            } else {
-                new_ask_order.qty -= best_bid.qty;
-
-                events.push(MatchEvent::TradeExecuted {
-                    maker_id: best_bid.id,
-                    taker_id: new_ask_order.id,
-                    price: new_ask_order.price,
-                    qty: best_bid.qty,
-                    timestamp: new_ask_order.timestamp,
-                });
-
-                self.bids.pop();
-            }
-        }
-
-        if new_ask_order.qty > 0 {
-            self.add_order_to_asks(new_ask_order, events);
-        }
     }
 
     fn match_new_market_bid(&mut self, mut new_bid_order: Order, events: &mut Vec<MatchEvent>) {
-        // Loop until:
-        // 1. Finish matching new_bid_order.qty == 0
-        // 2. Asks is empty
 
-        while new_bid_order.qty > 0 {
-            let best_ask = match self.asks.last_mut() {
-                Some(ask) => ask,
-                None => break,
-            };
 
-            if best_ask.qty > new_bid_order.qty {
-                best_ask.qty -= new_bid_order.qty;
-
-                events.push(MatchEvent::TradeExecuted {
-                    maker_id: best_ask.id,
-                    taker_id: new_bid_order.id,
-                    price: best_ask.price,
-                    qty: new_bid_order.qty,
-                    // ToDO: fix the timestamp to Unix Timestamp
-                    timestamp: new_bid_order.timestamp,
-                });
-
-                new_bid_order.qty = 0;
-
-                break;
-            } else {
-                new_bid_order.qty -= best_ask.qty;
-
-                events.push(MatchEvent::TradeExecuted {
-                    maker_id: best_ask.id,
-                    taker_id: new_bid_order.id,
-                    price: best_ask.price,
-                    qty: best_ask.qty,
-                    // ToDO: fix the timestamp to Unix Timestamp
-                    timestamp: new_bid_order.timestamp,
-                });
-
-                self.asks.pop();
-            }
-        }
-
-        // Notes: If there are remaining
-        // and the asks can't fulfil the bid.qty, just kill the order
-        // Standard Immediate-Or-Cancel approach
-        if new_bid_order.qty > 0 {
-            events.push(MatchEvent::OrderKilled {
-                id: new_bid_order.id,
-                killed_qty: new_bid_order.qty,
-            })
-        }
     }
 
     fn match_new_market_ask(&mut self, mut new_ask_order: Order, events: &mut Vec<MatchEvent>) {
-        // Loop until:
-        // 1. Finish matching new_ask_order.qty == 0
-        // 2. Bids is empty
 
-        while new_ask_order.qty > 0 {
-            let best_bid = match self.bids.last_mut() {
-                Some(bid) => bid,
-                None => break,
-            };
-
-            if best_bid.qty > new_ask_order.qty {
-                best_bid.qty -= new_ask_order.qty;
-
-                events.push(MatchEvent::TradeExecuted {
-                    maker_id: best_bid.id,
-                    taker_id: new_ask_order.id,
-                    price: best_bid.price,
-                    qty: new_ask_order.qty,
-                    timestamp: new_ask_order.timestamp,
-                });
-
-                new_ask_order.qty = 0;
-                break;
-            } else {
-                new_ask_order.qty -= best_bid.qty;
-
-                events.push(MatchEvent::TradeExecuted {
-                    maker_id: best_bid.id,
-                    taker_id: new_ask_order.id,
-                    price: best_bid.price,
-                    qty: best_bid.qty,
-                    timestamp: new_ask_order.timestamp,
-                });
-
-                self.bids.pop();
-            }
-        }
-
-        // Notes: If there are remaining
-        // and the asks can't fulfil the bid.qty, just kill the order
-        // Standard Immediate-Or-Cancel approach
-
-        if new_ask_order.qty > 0 {
-            events.push(MatchEvent::OrderKilled {
-                id: new_ask_order.id,
-                killed_qty: new_ask_order.qty,
-            })
-        }
     }
 
     pub fn add_order_to_bids(&mut self, new_bid_order: Order, events: &mut Vec<MatchEvent>) {
-        if new_bid_order.qty > 0 {
-            // Bids are sorted in Ascending order: [Smallest Price ... Highest Price].
-            // Example: [98, 99, 100(Old)]
-            // Reason: This allows O(1) popping of the Best Bid (Highest) from the end.
 
-            // --- FIFO Logic (Price-Time Priority) ---
-            // We need to insert the NEW order *before* the OLD order of the same price.
-            // Target Layout: [98, 99, 100(New), 100(Old)]
-            // This ensures that pop() retrieves 100(Old) first.
-
-            // 'partition_point' returns the index of the first element where the predicate is FALSE.
-            // Predicate: "Is x.price < new.price?"
-            // We want the first element where !(x < new), which implies x >= new.
-            let index = self.bids.partition_point(|x| x.price < new_bid_order.price);
-
-            self.bids.insert(index, OrderEntry::new(&new_bid_order));
-
-            events.push(MatchEvent::OrderPlaced {
-                id: new_bid_order.id,
-                price: new_bid_order.price,
-                qty: new_bid_order.qty,
-                side: new_bid_order.side,
-            });
-        }
     }
 
     pub fn add_order_to_asks(&mut self, new_ask_order: Order, events: &mut Vec<MatchEvent>) {
-        if new_ask_order.qty > 0 {
-            // Asks are sorted in Descending order: [Highest Price ... Smallest Price].
-            // Example: [102, 101, 100(Old)]
-            // Reason: This allows O(1) popping of the Best Ask (Lowest) from the end.
 
-            // --- FIFO Logic (Price-Time Priority) ---
-            // We need to insert the NEW order *before* the OLD order of the same price.
-            // Target Layout: [102, 101, 100(New), 100(Old)]
-            // This ensures that pop() retrieves 100(Old) first.
-
-            // 'partition_point' returns the index of the first element where the predicate is FALSE.
-            // Predicate: "Is x.price > new.price?"
-            // We want the first element where !(x > new), which implies x <= new.
-            // The loop continues as long as x.price < new_bid_order.price is true, and breaks immediately once it becomes false.
-            let index = self.asks.partition_point(|x| x.price > new_ask_order.price);
-
-            self.asks.insert(index, OrderEntry::new(&new_ask_order));
-
-            events.push(MatchEvent::OrderPlaced {
-                id: new_ask_order.id,
-                price: new_ask_order.price,
-                qty: new_ask_order.qty,
-                side: new_ask_order.side,
-            });
-        }
     }
 }
 
