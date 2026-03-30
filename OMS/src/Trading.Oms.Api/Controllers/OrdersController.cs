51a6b5186a26c1db18ec8306dac9046628e54033
AlexCKHO <alexho.ofl@gmail.com>
2026-03-30 13:25:22 +0100

    refactor(oms): orders controller

--- OMS/src/Trading.Oms.Api/Controllers/OrdersController.cs
+++ OMS/src/Trading.Oms.Api/Controllers/OrdersController.cs
@@ -10,37 +10,33 @@ public class OrdersController : ControllerBase
     [HttpPost]
     public IActionResult PlaceOrder(PlaceOrderRequest request)
     {
-        return Ok(new
-        {
-            status = "Order received!", Response = new CommandAckResponse(
-                RequestId: "1",
-                CorrelationId:"1",
-                IdempotencyKey:"1",
-                CommandType: CommandType.PlaceOrder,
-                Status: Status.Submitted,
-                OrderId: 123,
-                RejectionCode:null,
-                RejectionReason: null,
-                ReceivedAtUtc: DateTimeOffset.Now)
-        });
+        return Ok(new CommandAckResponse(
+            RequestId: "1",
+            CorrelationId: "1",
+            IdempotencyKey: "1",
+            CommandType: CommandType.PlaceOrder,
+            Status: Status.Submitted,
+            OrderId: 123,
+            RejectionCode: null,
+            RejectionReason: null,
+            ReceivedAtUtc: DateTimeOffset.Now)
+        );
     }
 
     [HttpPost]
-    [Route("/api/orders/cancel")]
-    public IActionResult CancelOrderRequest(CancelOrderRequest request)
+    [Route("cancel")]
+    public IActionResult CancelOrder(CancelOrderRequest request)
     {
-        return Ok(new
-        {
-            status = "Order received!", Response = new CommandAckResponse(
-                RequestId: "1",
-                CorrelationId:"1",
-                IdempotencyKey:"1",
-                CommandType: CommandType.CancelOrder,
-                Status: Status.Submitted,
-                OrderId: 123,
-                RejectionCode:null,
-                RejectionReason: null,
-                ReceivedAtUtc: DateTimeOffset.Now)
-        });
+        return Ok(new CommandAckResponse(
+            RequestId: "1",
+            CorrelationId: "1",
+            IdempotencyKey: "1",
+            CommandType: CommandType.CancelOrder,
+            Status: Status.Submitted,
+            OrderId: 123,
+            RejectionCode: null,
+            RejectionReason: null,
+            ReceivedAtUtc: DateTimeOffset.UtcNow)
+        );
     }
 }
