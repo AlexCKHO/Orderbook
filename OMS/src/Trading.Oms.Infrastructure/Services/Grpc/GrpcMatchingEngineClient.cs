502157683936c713f545ec3513532fadad966bb2
AlexCKHO <alexho.ofl@gmail.com>
2026-04-11 12:58:27 +0100

    Saving work

--- OMS/src/Trading.Oms.Infrastructure/Services/Grpc/GrpcMatchingEngineClient.cs
+++ OMS/src/Trading.Oms.Infrastructure/Services/Grpc/GrpcMatchingEngineClient.cs
@@ -1,6 +1,18 @@
+using Trading.Oms.Application.Commands;
+using Trading.Oms.Application.Interfaces;
+using Trading.Oms.Application.Models;
+
 namespace Trading.Oms.Infrastructure.Services.Grpc;
 
-public class GrpcMatchingEngineClient
+public class GrpcMatchingEngineClient : IMatchingEngineClient
 {
+    public Task<EnginePlaceOrderResult> PlaceOrderCommand(PlaceOrderCommand cmd, ulong orderId)
+    {
     
+    }
+
+    public Task<EngineCancelOrderResult> CancelOrderCommand(CancelOrderCommand cmd)
+    {
+        throw new NotImplementedException();
+    }
 }
