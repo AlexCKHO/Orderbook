a5a4f12d9f71a144bbb517e4b79e96fa2f91a3b2
AlexCKHO <alexho.ofl@gmail.com>
2026-04-22 00:16:16 +0100

    feat(broad): adding MarketHub

---
+++ MarketDataBroadcaster/Hubs/MarketHub.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.SignalR;
+
+namespace MarketDataBroadcaster.Hubs;
+
+public class MarketHub : Hub
+{
+    public async Task Subscribe(string symbol)
+    {
+        await Groups.AddToGroupAsync(Context.ConnectionId, symbol);
+        
+        Console.WriteLine($"Client {Context.ConnectionId} subscribed to {symbol}");
+    }
+}
